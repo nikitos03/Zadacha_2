80d3f770d5b00ef86cb3495e71de169c7b5573e6
nikitos03 <83069842+nikitos03@users.noreply.github.com>
2022-02-24 16:26:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ЕНГ_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст для выбора действия: ");
+
+            string text = Console.ReadLine();
+            while (!text.Contains("exit"))
+            {
+                if (text != "exit")
+                {
+                    Console.Write("Введите количество повторов сообщения: ");
+                    int povtor = int.Parse(Console.ReadLine());
+                    for (int i = 0; i < povtor; i++)
+                    {
+                        Console.WriteLine(text);
+                    }
+                    Console.WriteLine("Повторить операцию или exit для выхода");
+                    text = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
